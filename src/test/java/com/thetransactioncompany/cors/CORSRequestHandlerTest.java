73bc281b9fb13fa0191ee53baba45cdd282c5c9f
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-11-11 09:59:01 +0200

    Extends JUnit tests

---
+++ src/test/java/com/thetransactioncompany/cors/CORSRequestHandlerTest.java
@@ -0,0 +1,149 @@
+package com.thetransactioncompany.cors;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Tests the CORS request handler.
+ *
+ * @author Vladimir Dzhuvinov
+ */
+public class CORSRequestHandlerTest extends TestCase {
+
+
+	public void testActualRequestWithDefaultConfiguration()
+		throws Exception {
+
+		CORSConfiguration config = new CORSConfiguration(new Properties());
+
+		CORSRequestHandler handler = new CORSRequestHandler(config);
+
+		MockServletRequest request = new MockServletRequest();
+		request.setHeader("Origin", "http://example.com");
+
+		MockServletResponse response = new MockServletResponse();
+
+		handler.handleActualRequest(request, response);
+
+		assertEquals("http://example.com", response.getHeader("Access-Control-Allow-Origin"));
+
+		assertEquals("true", response.getHeader("Access-Control-Allow-Credentials"));
+
+		assertEquals(2, response.getHeaders().size());
+	}
+
+
+	public void testActualRequestWithExposedHeaders()
+		throws Exception {
+
+		Properties props = new Properties();
+		props.put("cors.exposedHeaders", "X-Custom");
+
+		CORSConfiguration config = new CORSConfiguration(props);
+
+		CORSRequestHandler handler = new CORSRequestHandler(config);
+
+		MockServletRequest request = new MockServletRequest();
+		request.setHeader("Origin", "http://example.com");
+
+		MockServletResponse response = new MockServletResponse();
+
+		handler.handleActualRequest(request, response);
+
+		assertEquals("http://example.com", response.getHeader("Access-Control-Allow-Origin"));
+
+		assertEquals("true", response.getHeader("Access-Control-Allow-Credentials"));
+
+		assertEquals("X-Custom", response.getHeader("Access-Control-Expose-Headers"));
+
+		assertEquals(3, response.getHeaders().size());
+	}
+
+
+	public void testActualRequestWithDeniedOrigin()
+		throws Exception {
+
+		Properties props = new Properties();
+		props.put("cors.allowOrigin", "http://example.com");
+
+		CORSConfiguration config = new CORSConfiguration(props);
+
+		CORSRequestHandler handler = new CORSRequestHandler(config);
+
+		MockServletRequest request = new MockServletRequest();
+		request.setHeader("Origin", "http://other.com");
+
+		MockServletResponse response = new MockServletResponse();
+
+		try {
+			handler.handleActualRequest(request, response);
+			fail();
+		} catch (CORSOriginDeniedException e) {
+			// ok
+			assertEquals("CORS origin denied", e.getMessage());
+		}
+	}
+
+
+	public void testActualRequestWithUnsupportedMethod()
+		throws Exception {
+
+		Properties props = new Properties();
+		props.put("cors.supportedMethods", "GET POST");
+
+		CORSConfiguration config = new CORSConfiguration(props);
+
+		CORSRequestHandler handler = new CORSRequestHandler(config);
+
+		MockServletRequest request = new MockServletRequest();
+		request.setHeader("Origin", "http://example.com");
+		request.setMethod("DELETE");
+
+		MockServletResponse response = new MockServletResponse();
+
+		try {
+			handler.handleActualRequest(request, response);
+			fail();
+		} catch (UnsupportedHTTPMethodException e) {
+			// ok
+			assertEquals("Unsupported HTTP method", e.getMessage());
+		}
+	}
+
+
+	public void testPreflightRequestWithDefaultConfiguration()
+		throws Exception {
+
+		CORSConfiguration config = new CORSConfiguration(new Properties());
+
+		CORSRequestHandler handler = new CORSRequestHandler(config);
+
+		MockServletRequest request = new MockServletRequest();
+		request.setHeader("Origin", "http://example.com");
+		request.setHeader("Access-Control-Request-Method", "POST");
+		request.setMethod("OPTIONS");
+
+		MockServletResponse response = new MockServletResponse();
+
+		handler.handlePreflightRequest(request, response);
+
+		assertEquals("http://example.com", response.getHeader("Access-Control-Allow-Origin"));
+
+		Set<String> methods = new HashSet<String>(Arrays.asList(HeaderUtils.parseMultipleHeaderValues(response.getHeader("Access-Control-Allow-Methods"))));
+		assertTrue(methods.contains("HEAD"));
+		assertTrue(methods.contains("GET"));
+		assertTrue(methods.contains("POST"));
+		assertTrue(methods.contains("OPTIONS"));
+		assertEquals(4, methods.size());
+
+		assertEquals("true", response.getHeader("Access-Control-Allow-Credentials"));
+
+		assertEquals(3, response.getHeaders().size());
+	}
+}
---
+++ src/test/java/com/thetransactioncompany/cors/MockServletResponse.java
@@ -0,0 +1,193 @@
+package com.thetransactioncompany.cors;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Mock servlet response.
+ *
+ * @author Vladimir Dzhuvinov
+ */
+public class MockServletResponse implements HttpServletResponse {
+
+
+	private int status = 200;
+
+
+	private final Map<String,String> headers = new HashMap<String, String>();
+
+
+	@Override
+	public void addCookie(Cookie cookie) {
+	}
+
+	@Override
+	public boolean containsHeader(String s) {
+		return false;
+	}
+
+	@Override
+	public String encodeURL(String s) {
+		return null;
+	}
+
+	@Override
+	public String encodeRedirectURL(String s) {
+		return null;
+	}
+
+	@Override
+	public String encodeUrl(String s) {
+		return null;
+	}
+
+	@Override
+	public String encodeRedirectUrl(String s) {
+		return null;
+	}
+
+	@Override
+	public void sendError(int i, String s) throws IOException {
+	}
+
+	@Override
+	public void sendError(int i) throws IOException {
+	}
+
+	@Override
+	public void sendRedirect(String s) throws IOException {
+	}
+
+	@Override
+	public void setDateHeader(String s, long l) {
+	}
+
+	@Override
+	public void addDateHeader(String s, long l) {
+	}
+
+
+	public String getHeader(final String name) {
+
+		return headers.get(name);
+	}
+
+
+	public Map<String,String> getHeaders() {
+
+		return headers;
+	}
+
+
+	@Override
+	public void setHeader(String name, String value) {
+
+		if (value == null)
+			headers.remove(name);
+		else
+			headers.put(name, value);
+	}
+
+	@Override
+	public void addHeader(String name, String value) {
+
+		headers.put(name, value);
+	}
+
+	@Override
+	public void setIntHeader(String s, int i) {
+	}
+
+	@Override
+	public void addIntHeader(String s, int i) {
+	}
+
+	public int getStatus() {
+
+		return status;
+	}
+
+	@Override
+	public void setStatus(int i) {
+	}
+
+	@Override
+	public void setStatus(int i, String s) {
+	}
+
+	@Override
+	public String getCharacterEncoding() {
+		return null;
+	}
+
+	@Override
+	public String getContentType() {
+		return null;
+	}
+
+	@Override
+	public ServletOutputStream getOutputStream() throws IOException {
+		return null;
+	}
+
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		return null;
+	}
+
+	@Override
+	public void setCharacterEncoding(String s) {
+	}
+
+	@Override
+	public void setContentLength(int i) {
+	}
+
+	@Override
+	public void setContentType(String s) {
+	}
+
+	@Override
+	public void setBufferSize(int i) {
+	}
+
+	@Override
+	public int getBufferSize() {
+		return 0;
+	}
+
+	@Override
+	public void flushBuffer() throws IOException {
+	}
+
+	@Override
+	public void resetBuffer() {
+	}
+
+	@Override
+	public boolean isCommitted() {
+		return false;
+	}
+
+	@Override
+	public void reset() {
+	}
+
+	@Override
+	public void setLocale(Locale locale) {
+	}
+
+	@Override
+	public Locale getLocale() {
+		return null;
+	}
+}
